6d8950657a42aa209c37315e2de8cfcff2b74230
Administrator <Administrator@304-10>
2024-01-12 15:30:28 +0900

    startbullti

---
+++ src/main/java/com/nowon/bul/SpringbootBulltiApplication.java
@@ -0,0 +1,13 @@
+package com.nowon.bul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBulltiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBulltiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nowon/bul/controller/DummyController.java
@@ -0,0 +1,5 @@
+package com.nowon.bul.controller;
+
+public class DummyController {
+
+}
