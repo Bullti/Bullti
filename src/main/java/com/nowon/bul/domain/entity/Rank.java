f936bb6d1501b9f964f9322cfec96d37a90fc0d7
최영진 <Administrator@304-3>
2024-01-16 16:55:13 +0900

    Security Session 등록

---
+++ src/main/java/com/nowon/bul/domain/entity/Rank.java
@@ -0,0 +1,14 @@
+package com.nowon.bul.domain.entity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Rank {
+	
+	Assistant("사원"),
+	SeniorAssistant("대리"),
+	Manager("과장"),
+	SeniorManager("부장");
+	
+	private final String rankName;
+}
---
+++ src/main/java/com/nowon/bul/domain/entity/Role.java
@@ -0,0 +1,14 @@
+package com.nowon.bul.domain.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+	
+	USER("일반유저"),
+	FR("프렌차이즈");
+	
+	private final String roleName;
+}
