37257078042e726c61e0f6616790ae96dc3b2ede
chj303 <chj303-2023VERFI>
2024-01-17 19:57:05 +0900

    table making by chj

---
+++ src/main/java/com/nowon/bul/stock/service/impl/ProductServiceImpl.java
@@ -0,0 +1,11 @@
+package com.nowon.bul.stock.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.nowon.bul.stock.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+
+}
