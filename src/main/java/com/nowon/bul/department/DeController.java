4548a909592acd7b1bf6d45a447a3587c9c0d02c
이예슬 <Administrator@304-4>
2024-01-17 17:52:05 +0900

    0117 - 3

--- src/main/java/com/nowon/bul/department/DeController.java
+++ src/main/java/com/nowon/bul/department/DeController.java
@@ -1,7 +1,11 @@
 package com.nowon.bul.department;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,11 +15,17 @@ public class DeController {
 	DeService deService;
 	
 	@GetMapping("/depart")
-	public String deSave() {
-		
-		
-		
-		return "department/save";
-	}
+	public String delist() {
 
-}
+		return "department/temp";
+	 }
+	
+	 @GetMapping("/depart/list")
+	    public String showOrgChart(Model model) {
+		 	System.out.println("이건 맞아?");
+	        List<Map<String, String>> orgChartData = deService.getOrgChartData();
+	        model.addAttribute("orgChartData", orgChartData);
+	        return "department/deList";
+
+	 }
+}	 
--- src/main/java/com/nowon/bul/department/DeService.java
+++ src/main/java/com/nowon/bul/department/DeService.java
@@ -1,9 +1,14 @@
 package com.nowon.bul.department;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 
 public interface DeService {
+
+	List<Map<String, String>> getOrgChartData();
 }
