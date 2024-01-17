20b5751d0b8b9e184ad3f735e7d3928ef9aac636
이예슬 <Administrator@304-4>
2024-01-17 17:52:05 +0900

    0117 - 3

--- src/main/java/com/nowon/bul/department/DeEntity.java
+++ src/main/java/com/nowon/bul/department/DeEntity.java
@@ -1,12 +1,20 @@
 package com.nowon.bul.department;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 
 @Entity
 @NoArgsConstructor
@@ -15,8 +23,26 @@ import lombok.NoArgsConstructor;
 @Getter
 public class DeEntity {
 
-	@Id
-	private int deptId;
-	@Column
-	private String deptName;
+    @Id
+    @Column(name = "dept_id")
+    private int deptId;
+
+    @Column
+    private String deptName;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = DeEntity.class)
+    @JoinColumn(name = "parent_dept_id", referencedColumnName = "dept_id", insertable = false, updatable = false)
+    private DeEntity parent;
+    
+    @OneToMany(mappedBy = "parent")
+    private List<DeEntity> 	child;
+    
+    
+    
+    public DeListDTO toListDTO() {
+		return DeListDTO.builder()
+				.deptId(deptId)
+				.deptName(deptName)
+				.build();
+    }
 }
--- src/main/java/com/nowon/bul/department/DeServiceImpe.java
+++ src/main/java/com/nowon/bul/department/DeServiceImpe.java
@@ -1,7 +1,13 @@
 package com.nowon.bul.department;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DeServiceImpe implements DeService {
@@ -16,6 +22,31 @@ public class DeServiceImpe implements DeService {
 	
 	
 	/* 부서 조회 */
+	
+		@Transactional
+		@Override
+	    public List<Map<String, String>> getOrgChartData() {
+	        List<Map<String, String>> orgChartData = new ArrayList<>();
+	        System.out.println("바보세요?");
+	        // 데이터베이스에서 정보를 가져와서 orgChartData에 추가하는 로직
+	        List<DeEntity> departments = deRepository.findAll();
+	        for (DeEntity department : departments) {
+	        	System.out.println(">>>>>>>>>>"+department.toString());
+	        	System.out.println(">>>>>>>>>>"+department.toString());
+	        	System.out.println(">>>>>>>>>>"+department.toString());
+	            Map<String, String> node = new HashMap<>();
+	            node.put("name", department.getDeptName());
+	            if(department.getParent() != null) {
+	            	node.put("manager", department.getParent().getDeptName()); // 여기서 적절한 매니저 정보를 가져오는 로직 추가
+	            }else {
+	            	node.put("manager", "");
+	            }
+	            orgChartData.add(node);
+	            
+	        }
+
+	        return orgChartData;
+	    }
 	
 	/*
 	 * public String deList(DeEntity deEntity) { deRepository.findAll(); }
