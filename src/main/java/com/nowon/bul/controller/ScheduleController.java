6cd4eb6d36f18d7ba7931d17e31dd0be355b6d31
Administrator <Administrator@host.docker.internal>
2024-01-18 10:00:55 +0900

    01181000

--- src/main/java/com/nowon/bul/controller/ScheduleController.java
+++ src/main/java/com/nowon/bul/controller/ScheduleController.java
@@ -12,6 +12,6 @@ public class ScheduleController {
 
 	@GetMapping
 	public String attendanceList(Authentication auth, Model model) {
-		return "views/emp/atten/list";
+		return "views/emp/atten/calendar";
 	}
 }
---
+++ src/main/resources/static/js/calendar/fullcalendar.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const calendarEl = document.getElementById('calendar')
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+  })
+  calendar.render()
+})
+
