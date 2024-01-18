9e14810777058546a0597692d4122e481e0b4caa
chj <kiiieo@naver.com>
2024-01-18 14:45:41 +0900

    상품 현황 css,html

--- src/main/java/com/nowon/bul/stock/service/ProductService.java
+++ src/main/java/com/nowon/bul/stock/service/ProductService.java
@@ -1,5 +1,10 @@
 package com.nowon.bul.stock.service;
 
-public interface ProductService {
+import java.util.List;
 
+import com.nowon.bul.stock.dto.ProductDTO;
+
+public interface ProductService {
+	
+	List<ProductDTO> getAllProducts();
 }
