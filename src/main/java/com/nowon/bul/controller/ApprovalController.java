5e8aef12e66795d69a2be8f98efdb2d7a7b1ff89
Administrator <Administrator@304-3>
2024-01-19 17:58:22 +0900

    2024.01.19 18:00

--- src/main/java/com/nowon/bul/controller/ApprovalController.java
+++ src/main/java/com/nowon/bul/controller/ApprovalController.java
@@ -1,20 +1,37 @@
 package com.nowon.bul.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.nowon.bul.department.DeService;
+import com.nowon.bul.service.ApprovalService;
+
+import ch.qos.logback.core.model.Model;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RequestMapping("/approval")
 @Controller
 public class ApprovalController {
 
+	
+	private final ApprovalService approvalService;
+	
+	private final DeService deService;
+	
 	@GetMapping("")
 	public String approvalModal() {
 		return "/views/approval/home";
 	}
 	
 	@GetMapping("/write")
-	public String writePage() {
+	public String writePage(Model model) {
+		//List<Map<String, Object>> deptList = deService 
+		
 		
 		return "/views/approval/write";
 	}
