264d09d4b6e89b1603149692879ee2824be71075
chj <kiiieo@naver.com>
2024-01-19 17:53:13 +0900

    2024_01_19 chj

--- src/main/java/com/nowon/bul/stock/controller/StockController.java
+++ src/main/java/com/nowon/bul/stock/controller/StockController.java
@@ -1,13 +1,21 @@
 package com.nowon.bul.stock.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.nowon.bul.stock.service.ProductService;
 
 @Controller
 public class StockController {
 	
+	@Autowired
+	private ProductService productService;
+	
 	@GetMapping("/members/stock")
-	public String stock() {
+	public String stock(Model model) {
+		model.addAttribute("products",productService.getAllProducts());
 		return "stock/stock";
 	}
 	
