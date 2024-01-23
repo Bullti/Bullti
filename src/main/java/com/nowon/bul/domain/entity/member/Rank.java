6a23795e0bb704da0db2d228679c19b552a7b8f2
Administrator <Administrator@User -2023CCUNM>
2024-01-23 21:34:22 +0900

    2024.01.23 21:34

--- src/main/java/com/nowon/bul/domain/entity/member/Rank.java
+++ src/main/java/com/nowon/bul/domain/entity/member/Rank.java
@@ -8,7 +8,8 @@ public enum Rank {
 	Assistant("사원"),
 	SeniorAssistant("대리"),
 	Manager("과장"),
-	SeniorManager("부장");
+	SeniorManager("부장"),
+	StoreManager("점장");
 	
 	private final String rankName;
 }
