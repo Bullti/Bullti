eb36450d4d9537358a3b60b1de68f5686c3472f6
Your Name <you@example.com>
2024-01-24 12:44:37 +0900

    myUser add getter

--- src/test/java/com/nowon/bul/kbwApplicationTest.java
+++ src/test/java/com/nowon/bul/kbwApplicationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.nowon.bul.domain.dto.attendance.AttendanceCheckDTO;
 import com.nowon.bul.mybatis.mapper.AttendanceMapper;
 
 @SpringBootTest
@@ -14,8 +15,10 @@ class kbwApplicationTest {
 
 	//@Test
 	void workinchecktest() {
+		AttendanceCheckDTO dto = new AttendanceCheckDTO();
+		dto.setMemberNo(6L);
+		dto.setRegiType(1);
 		for(int i=0; i<3; i++)
-			mapper.workIn(5959L);
-		
+			mapper.workIn(dto);
 	}
 }
