5c32d7be39754ebd67d663389c871dd640bb17b6
Your Name <you@example.com>
2024-01-24 12:44:37 +0900

    myUser add getter

--- src/main/java/com/nowon/bul/mybatis/mapper/AttendanceMapper.java
+++ src/main/java/com/nowon/bul/mybatis/mapper/AttendanceMapper.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.nowon.bul.domain.dto.AttendanceDTO;
+import com.nowon.bul.domain.dto.attendance.AttendanceCheckDTO;
 
 @Mapper
 public interface AttendanceMapper {
 
-	void workIn(Long memberNo);
+	void workIn(AttendanceCheckDTO dto);
 
 	List<AttendanceDTO> find(Long memberNo);
 	
