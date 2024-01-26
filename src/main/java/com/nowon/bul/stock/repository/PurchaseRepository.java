ff0330067fb5d4f3f5ccd943138b453b4c9809a5
chj <kiiieo@naver.com>
2024-01-26 21:36:47 +0900

    240126

--- src/main/java/com/nowon/bul/stock/repository/PurchaseRepository.java
+++ src/main/java/com/nowon/bul/stock/repository/PurchaseRepository.java
@@ -1,9 +1,16 @@
 package com.nowon.bul.stock.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nowon.bul.stock.entity.PurchaseEntity;
 
 public interface PurchaseRepository extends JpaRepository<PurchaseEntity, Integer>{
 
+	@Query("SELECT p FROM PurchaseEntity p JOIN FETCH p.product JOIN FETCH p.franchise")
+    List<PurchaseEntity> getAllPurchases();
+	
+	
 }
