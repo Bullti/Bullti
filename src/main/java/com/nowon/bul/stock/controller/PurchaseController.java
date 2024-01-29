096e02907a51d313cde514a200bfcf0ee5a4013f
chj <kiiieo@naver.com>
2024-01-29 17:54:10 +0900

    240119

--- src/main/java/com/nowon/bul/stock/controller/PurchaseController.java
+++ src/main/java/com/nowon/bul/stock/controller/PurchaseController.java
@@ -3,9 +3,13 @@ package com.nowon.bul.stock.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,6 +79,16 @@ public class PurchaseController {
 	}
 
 	
+	//상품 삭제
+	 @DeleteMapping("/members/purchase/{purchaseNum}")
+	    public ResponseEntity<?> deletePurchase(@PathVariable("purchaseNum") int purchaseNum) {
+	        try {
+	            purchaseService.deletePurchase(purchaseNum);
+	            return new ResponseEntity<>(HttpStatus.OK);
+	        } catch (Exception e) {
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
+	    }
 	
 	
 	
