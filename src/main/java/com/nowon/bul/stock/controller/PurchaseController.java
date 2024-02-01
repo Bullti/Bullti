07c2e3ffb31790b05d791364d1f756768d5df042
chj <kiiieo@naver.com>
2024-02-01 18:34:38 +0900

    240201 commit

--- src/main/java/com/nowon/bul/stock/controller/PurchaseController.java
+++ src/main/java/com/nowon/bul/stock/controller/PurchaseController.java
@@ -1,19 +1,9 @@
 package com.nowon.bul.stock.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.nowon.bul.stock.dto.PurchaseDTO;
 import com.nowon.bul.stock.entity.ProductEntity;
@@ -23,73 +13,61 @@ import com.nowon.bul.stock.service.ProductService;
 import com.nowon.bul.stock.service.PurchaseService;
 
 @Controller
+@RequestMapping("/members")  // 클래스 레벨에서 공통 경로 설정
 public class PurchaseController {
-	
-	@Autowired
-	private ProductService productService;
-	
-	@Autowired
-	private PurchaseService purchaseService;
-	
-	@Autowired
-	private PurchaseRepository purchaseRepository;
-	
-	
-	@GetMapping("/members/purchase")
-	public String purchase(Model model) {
-		model.addAttribute("products", productService.getAllProducts());
-		return "stock/purchase";
-	}
-	
-	
-	
-	@GetMapping("/members/purchase-post")
-	public String purchase_post(Model model) {
-	 
-	   
-	    model.addAttribute("purchases", purchaseService.getAllPurchases());
-	    return "stock/purchase-post";
-	}
-	
-	@ResponseBody
-	@PostMapping("/members/purchase-post")
-	public String purchase_post(@RequestBody PurchaseDTO dto) {
-	    System.out.println(">>>>" + dto.getProductName());
-	    System.out.println(">>>>" + dto.getEa());
+    
+    @Autowired
+    private ProductService productService;
+    
+    @Autowired
+    private PurchaseService purchaseService;
+    
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+    
+    
+    @GetMapping("/purchase")
+    public String purchase(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "stock/purchase";
+    }
+    
+    @GetMapping("/purchase-post")
+    public String purchase_post(Model model) {
+        model.addAttribute("purchases", purchaseService.getAllPurchases());
+        return "stock/purchase-post";
+    }
+    
+    @ResponseBody
+    @PostMapping("/purchase-post")
+    public String purchase_post(@RequestBody PurchaseDTO dto) {
+        System.out.println(">>>>" + dto.getProductName());
+        System.out.println(">>>>" + dto.getEa());
+    
+        // 상품 조회
+        ProductEntity product = productService.getProductByName(dto.getProductName());
+    
+        // PurchaseEntity 생성
+        PurchaseEntity purchaseEntity = PurchaseEntity.builder()
+                .product(product)
+                .ea(dto.getEa())
+                .build();
+    
+        // 생성된 PurchaseEntity를 DB에 저장
+        purchaseRepository.save(purchaseEntity);
+    
+        return "등록 성공";
+    }
+    
+    @GetMapping("/purchase-complete")
+    public String purchase_complete() {
+        return "stock/purchase-complete";
+    }
 
-	    // 상품 조회
-	    ProductEntity product = productService.getProductByName(dto.getProductName());
-
-	    // PurchaseEntity 생성
-	    PurchaseEntity purchaseEntity = PurchaseEntity.builder()
-	            .product(product)
-	            .ea(dto.getEa())
-	            .build();
-
-	    // 생성된 PurchaseEntity를 DB에 저장
-	    purchaseRepository.save(purchaseEntity);
-
-	    return "등록성공";
-	}
-	
-	@GetMapping("/members/purchase-complete")
-	public String purchase_complete() {
-		
-		return "stock/purchase-complete";
-	}
-
-	
-	//상품 삭제
-	 @DeleteMapping("/members/purchase/{purchaseNum}")
-	    public ResponseEntity<?> deletePurchase(@PathVariable("purchaseNum") int purchaseNum) {
-	        try {
-	            purchaseService.deletePurchase(purchaseNum);
-	            return new ResponseEntity<>(HttpStatus.OK);
-	        } catch (Exception e) {
-	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	        }
-	    }
-	
-	
-	
+    // 상품 삭제
+    @DeleteMapping("/purchase-post/{purchaseNum}")
+    public String deletePurchase(@PathVariable("purchaseNum") int purchaseNum) {
+        purchaseService.deletebyid(purchaseNum);
+        return "redirect:/members/purchase-post";  // 삭제 후 목록으로 리다이렉션
+    }
 }
