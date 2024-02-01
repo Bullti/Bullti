5c4f0923e76a8e97c94a3d71e9c4a0abd9f61917
spmqjl <spmjql@example.com>
2024-02-01 23:33:38 +0900

    휴가결재연동 완료

--- src/main/java/com/nowon/bul/domain/dto/AnnualSaveDTO.java
+++ src/main/java/com/nowon/bul/domain/dto/AnnualSaveDTO.java
@@ -1,12 +1,12 @@
 package com.nowon.bul.domain.dto;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@ToString
 @Setter
 @Getter
 public class AnnualSaveDTO {
@@ -18,4 +18,9 @@ public class AnnualSaveDTO {
 	private String halfDayTime;
 	private String head;
 	private String content;
+	private int approve;
+	
+	public long daysDifference() {
+		return ChronoUnit.DAYS.between(start, end);
+	}
 }
