c9788361095b28e5e2bde2c5f571d21c3e4306ec
Your Name <you@example.com>
2024-02-02 17:56:32 +0900

    휴가 이넘 추가

--- src/main/java/com/nowon/bul/domain/dto/annual/AnnualListDTO.java
+++ src/main/java/com/nowon/bul/domain/dto/annual/AnnualListDTO.java
@@ -8,18 +8,20 @@ import lombok.Getter;
 public class AnnualListDTO {
 
 	private long annualNo;
-	private String type;
+	private int type;
 	private long memberNo;
 	private String content;
 	private LocalDateTime start;
 	private LocalDateTime end;
 	private LocalDateTime createDate;
 	private String halfDayTime;
-	private String head;
 	private int approve;
 	
     // Enum에 대응하는 문자열 반환 메서드
     public String getApprove() {
         return AnnualApproveCode.valueOfNumber(approve).getApproveState();
+    }
+    public String getType() {
+        return AnnualType.valueOfNumber(type).getTypeName();
     }
 }
