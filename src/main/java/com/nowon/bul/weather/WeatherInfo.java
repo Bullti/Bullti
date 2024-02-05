e10e94865b611bd5bd1a7d68c9aaf10c136af990
SB <Administrator@304-11>
2024-02-05 17:49:35 +0900

    2024 02 05

---
+++ src/main/java/com/nowon/bul/weather/WeatherInfo.java
@@ -0,0 +1,13 @@
+package com.nowon.bul.weather;
+
+import lombok.Data;
+
+@Data
+public class WeatherInfo {
+	
+	private String lat;
+	private String lng;
+	private String x;
+	private String y;
+
+}
